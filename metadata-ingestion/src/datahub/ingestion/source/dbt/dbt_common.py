91f6b60c485d7296722b92aef59580585b7e04fd
Daniel Messias <danielcmessias@gmail.com>
2023-09-25 08:56:19 +0100

    More WIP

--- metadata-ingestion/src/datahub/ingestion/source/dbt/dbt_common.py
+++ metadata-ingestion/src/datahub/ingestion/source/dbt/dbt_common.py
@@ -944,23 +944,19 @@ class DBTSourceBase(StatefulIngestionSourceBase):
             self.config.platform_instance,
         )
 
-        # yield from self.create_platform_mces(
-        #     non_test_nodes,
-        #     additional_custom_props_filtered,
-        #     all_nodes_map,
-        #     self.config.target_platform,
-        #     self.config.target_platform_instance,
-        # )
+        yield from self.create_platform_mces(
+            non_test_nodes,
+            additional_custom_props_filtered,
+            all_nodes_map,
+            self.config.target_platform,
+            self.config.target_platform_instance,
+        )
 
         # yield from self.create_test_entity_mcps(
         #     test_nodes,
         #     additional_custom_props_filtered,
         #     all_nodes_map,
         # )
-
-        # test: emit column lineage
-        # if self.config.parse_sql_lineage:
-        #     yield from self.create_lineage_mces(non_test_nodes)
 
     def filter_nodes(self, all_nodes: List[DBTNode]) -> List[DBTNode]:
         nodes = []
@@ -1092,64 +1088,6 @@ class DBTSourceBase(StatefulIngestionSourceBase):
             yield MetadataWorkUnit(
                 id=dataset_snapshot.urn, mce=mce, is_primary_source=is_primary_source
             )
-
-    def create_lineage_mces(self, nodes) -> Iterable[MetadataWorkUnit]:
-        builder = SqlParsingBuilder(
-        #    generate_lineage=True,
-        #    generate_usage_statistics=False,
-        #    generate_operations=False, 
-        )
-        for node in nodes:
-            # if "application_flow" not in node.name:
-            #     continue
-
-            if not node.compiled_code:
-                # raise Exception(f"No compiled code for {node.name}")
-                continue
-
-            # Enclose the query in a CTAS. The real CTAS-query depends on the adapter (and isn't
-            # included in the manifest), but we don't need it. All we really need to do is let
-            # sqlglot know that this query has a downstream. Better ideas welcome here :)
-            query = f"CREATE TABLE {node.database}.{node.schema}.{node.name} AS {node.compiled_code}"
-            result = sqlglot_lineage(
-                sql=query,
-                schema_resolver=self.schema_resolver,
-            )
-
-            # logger.info(query)
-            # logger.info("*************")
-            # logger.info(result)
-            # raise Exception("See above")
-
-            if result.debug_info.table_error:
-                logger.info(f"Error parsing table lineage, {result.debug_info.table_error}")
-            elif result.debug_info.column_error:
-                logger.info(
-                    f"Error parsing column lineage, {result.debug_info.column_error}"
-                )
-
-            for cl in result.column_lineage or []:
-                logger.info(f"\n{cl.downstream}\n{cl.upstreams}\n{cl.logic}")
-
-            # This works, so long as manually prefix the CTAS statement.  
-            # NOTE: Since this returns an iterator we need to convert to force evaluation or yield
-            list(builder.process_sql_parsing_result(result, query=query))
-            # yield from list(builder.process_sql_parsing_result(
-            #     result,
-            #     query=query,
-            #     query_timestamp=datetime.now(),
-            #     user="urn:li:corpuser:datahub",
-            #     # custom_operation_type=entry.operation_type,
-            #     # include_urns=self.urns,
-            # ))
-        
-        # res = list(builder.gen_workunits())
-        # if len(res) == 0:
-        #     raise Exception("GOT NOTHING :(")
-        # for r in res:
-        #     logging.info(f"Generated workunit: {r}")
-
-        yield from builder.gen_workunits()
 
     def extract_query_tag_aspects(
         self,
@@ -1479,11 +1417,8 @@ class DBTSourceBase(StatefulIngestionSourceBase):
             if self.config.enable_sql_parsing:
                 fine_grained_lineages = self._create_fine_grained_lineage(node, all_nodes_map)
 
-                if node.name == "daniel_messias__gpo_offer_flow__cases_latest":
-                    from pprint import pformat
-                    raise Exception(pformat(fine_grained_lineages))
-
             return get_upstream_lineage(upstream_urns, fine_grained_lineages)
+
         return None
 
     def _create_fine_grained_lineage(
